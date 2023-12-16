4d9f4e5d259bda8ab13e6b85a8d44095669486bf
“Joshua-romil” <joshuaromil@gmail.com>
2023-12-16 15:22:17 +0100

    Refactored code so that carousel cell is now seperate compared to before when I only had one type of cell to work with.

---
+++ RocketWatch/View/FeaturedCarouselCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  FeaturedCarouselCollectionViewCell.swift
+//  RocketWatch
+//
+//  Created by Joshua George on 2023-12-16.
+//
+
+import UIKit
+
+class FeaturedCarouselCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
