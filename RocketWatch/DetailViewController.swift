6854262015abe446549d99bf84d04e97021c0641
Joshua-romil <joshuaromil@gmail.com>
2025-03-04 20:09:09 +0100

    Added support for map view to the detail view. Also refactored the detail view to work a bit more dynamic depending on type of content to be displayed.

--- RocketWatch/DetailViewController.swift
+++ RocketWatch/DetailViewController.swift
@@ -7,8 +7,14 @@
 
 import UIKit
 
+enum DetailType{
+    case rocket
+    case ship
+}
+
 class DetailViewController: UIViewController {
     
+    var detailType: DetailType = .rocket
     
     @IBOutlet weak var detailCollectionView: UICollectionView!
     
@@ -17,6 +23,7 @@ class DetailViewController: UIViewController {
         "It was first launched in June 2010 and has since become a key player in commercial spaceflight.",
         "The rocket is known for its first-stage booster landings, which enable cost reduction in space missions."
     ]
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,9 +38,16 @@ class DetailViewController: UIViewController {
         registerAboutCell()
         registerKeyFactsCell()
         registerPicturesHeader()
+        registerMapCell()
         
         detailCollectionView.dataSource = self
         detailCollectionView.delegate = self
+        
+        print("Vilket cell typ har användare klickat på?: \(detailType)")
+        
+        if detailType == .ship{
+            //TODO: Hide sections from the detailview that you don't want for ships
+        }
         
     }
     
@@ -63,49 +77,55 @@ class DetailViewController: UIViewController {
         self.detailCollectionView.register(UICollectionReusableView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "HeaderView")
     }
     
+    private func registerMapCell(){
+        self.detailCollectionView.register(UINib(nibName: "MapCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "MapCollectionViewCell")
+    }
+    
     func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 5 // Hero, Status indicator, About, Key Facts, Pictures
+        
+        switch detailType {
+        case .rocket:
+            return 5 // Hero, Status indicator, About, Key Facts, Pictures
+        case .ship:
+            return 4 // Hero, Status indicator, Key Facts, Map
+        }
+        
     }
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         switch section {
-        case 0: // Hero
-            return 1
-        case 1: // Status indicator
-            return 1
-        case 2: // About
-            return 1
-        case 3: // Key Facts
-            return 1
-        case 4:
-            return 4 // However many pictures you have
-        default:
-            return 0
+            case 0: return 1 // Hero
+            case 1: return 1 // Status indicator
+            case 2: return 1 // About
+            case 3: return 1 // Key Facts
+            case 4: return 4 // However many pictures you have
+            case 5: return 1 // Map
+            default: return 0
         }
     }
     
     private func createLayout() -> UICollectionViewCompositionalLayout{
         return UICollectionViewCompositionalLayout { (sectionIndex, layoutEnvironment) -> NSCollectionLayoutSection? in
+            
+            if self.detailType == .rocket{
                 switch sectionIndex {
-                case 0:
-                    // Hero section
-                    return self.createHeroSection()
-                case 1:
-                    // Status section
-                    return self.createStatusIndicatorSection()
-                case 2:
-                    // About section
-                    return self.createAboutSection()
-                case 3:
-                    // Key facts section
-                    return self.createKeyFactsSection()
-                case 4:
-                    // Pictures section
-                    return self.createPicturesSection()
-                default:
-                    return nil
+                    case 0: return self.createHeroSection()
+                    case 1: return self.createStatusIndicatorSection()
+                    case 2: return self.createAboutSection()
+                    case 3: return self.createKeyFactsSection()
+                    case 4: return self.createPicturesSection()
+                    default: return nil
+                }
+            } else {
+                switch sectionIndex {
+                    case 0: return self.createHeroSection()
+                    case 1: return self.createStatusIndicatorSection()
+                    case 2: return self.createKeyFactsSection()
+                    case 3: return self.createMapSection()
+                    default: return nil
                 }
             }
+        }
     }
     
     private func createHeroSection() -> NSCollectionLayoutSection {
@@ -208,83 +228,147 @@ class DetailViewController: UIViewController {
         
         return section
     }
+    
+    private func createMapSection() -> NSCollectionLayoutSection {
+        
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(250))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),heightDimension: .absolute(250))
+        let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
+            
+        let section = NSCollectionLayoutSection(group: group)
+        
+        section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 16, bottom: 20, trailing: 16)
+        
+        //Label over section
+        let sectionHeaderSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(40))
+        
+        let header = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: sectionHeaderSize, elementKind: UICollectionView.elementKindSectionHeader, alignment: .topLeading)
+        
+        section.boundarySupplementaryItems = [header]
+        
+        return section
+        
+    }
 
 }
 
 extension DetailViewController: UICollectionViewDataSource,UICollectionViewDelegate {
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    
         
-        switch indexPath.section {
-        case 0:
-            let carouselCell = detailCollectionView.dequeueReusableCell(withReuseIdentifier: "FeaturedCarouselCollectionViewCell", for: indexPath) as! FeaturedCarouselCollectionViewCell
-            carouselCell.carouselImage.image = UIImage(named: "Falcon1")
-            carouselCell.title.isHidden = true
-            carouselCell.subtitle.isHidden = true
-            return carouselCell
-        case 1:
-            let statusCell = detailCollectionView.dequeueReusableCell(withReuseIdentifier: "StatusIndicatorCollectionViewCell", for: indexPath) as! StatusIndicatorCollectionViewCell
-            return statusCell
-        case 2:
-            let aboutCell = detailCollectionView.dequeueReusableCell(withReuseIdentifier: "AboutCollectionViewCell", for: indexPath) as! AboutCollectionViewCell
-            return aboutCell
-        case 3:
-            let keyFactsCell = detailCollectionView.dequeueReusableCell(withReuseIdentifier: "KeyFactsCollectionViewCell", for: indexPath) as! KeyFactsCollectionViewCell
+        switch detailType {
+        case .rocket:
             
-            let keyFacts: [(field: String, fact: String)] = [
-                        ("First Flight", "June 4, 2010"),
-                        ("Height", "70 m"),
-                        ("Diameter", "3.7 m"),
-                        ("Mass", "549,054 kg"),
-                        ("Stages", "2"),
-                        ("Payload to LEO", "22,800 kg")
-                    ]
+            switch indexPath.section {
+            case 0:
+                let carouselCell = detailCollectionView.dequeueReusableCell(withReuseIdentifier: "FeaturedCarouselCollectionViewCell", for: indexPath) as! FeaturedCarouselCollectionViewCell
+                carouselCell.carouselImage.image = UIImage(named: "Falcon1")
+                carouselCell.title.isHidden = true
+                carouselCell.subtitle.isHidden = true
+                return carouselCell
+            case 1:
+                let statusCell = detailCollectionView.dequeueReusableCell(withReuseIdentifier: "StatusIndicatorCollectionViewCell", for: indexPath) as! StatusIndicatorCollectionViewCell
+                return statusCell
+            case 2:
+                let aboutCell = detailCollectionView.dequeueReusableCell(withReuseIdentifier: "AboutCollectionViewCell", for: indexPath) as! AboutCollectionViewCell
+                return aboutCell
+            case 3:
+                let keyFactsCell = detailCollectionView.dequeueReusableCell(withReuseIdentifier: "KeyFactsCollectionViewCell", for: indexPath) as! KeyFactsCollectionViewCell
+                let keyFacts: [(field: String, fact: String)] = [
+                            ("First Flight", "June 4, 2010"),
+                            ("Height", "70 m"),
+                            ("Diameter", "3.7 m"),
+                            ("Mass", "549,054 kg"),
+                            ("Stages", "2"),
+                            ("Payload to LEO", "22,800 kg")
+                        ]
+                
+                keyFactsCell.configure(with: keyFacts)
+                
+                return keyFactsCell
+            case 4:
+                let carouselCell = detailCollectionView.dequeueReusableCell(withReuseIdentifier: "FeaturedCarouselCollectionViewCell", for: indexPath) as! FeaturedCarouselCollectionViewCell
+                carouselCell.carouselImage.image = UIImage(named: "Falcon1")
+                carouselCell.title.isHidden = true
+                carouselCell.subtitle.isHidden = true
+                carouselCell.layer.cornerRadius = 8
+                return carouselCell
+            default:
+                //this will be replaced anyway
+                let carouselCell = detailCollectionView.dequeueReusableCell(withReuseIdentifier: "FeaturedCarouselCollectionViewCell", for: indexPath) as! FeaturedCarouselCollectionViewCell
+                carouselCell.carouselImage.image = UIImage(named: "Falcon1")
+                carouselCell.title.text = "hello world"
+                carouselCell.subtitle.text = "hello world"
+                return carouselCell
+            }
             
-            keyFactsCell.configure(with: keyFacts)
             
-            return keyFactsCell
-        case 4:
-            let carouselCell = detailCollectionView.dequeueReusableCell(withReuseIdentifier: "FeaturedCarouselCollectionViewCell", for: indexPath) as! FeaturedCarouselCollectionViewCell
-            carouselCell.carouselImage.image = UIImage(named: "Falcon1")
-            carouselCell.title.isHidden = true
-            carouselCell.subtitle.isHidden = true
-            carouselCell.layer.cornerRadius = 8
-            return carouselCell
+        case .ship:
+            
+            switch indexPath.section {
+            case 0:
+                let carouselCell = detailCollectionView.dequeueReusableCell(withReuseIdentifier: "FeaturedCarouselCollectionViewCell", for: indexPath) as! FeaturedCarouselCollectionViewCell
+                carouselCell.carouselImage.image = UIImage(named: "Falcon1")
+                carouselCell.title.isHidden = true
+                carouselCell.subtitle.isHidden = true
+                return carouselCell
+            case 1:
+                let statusCell = detailCollectionView.dequeueReusableCell(withReuseIdentifier: "StatusIndicatorCollectionViewCell", for: indexPath) as! StatusIndicatorCollectionViewCell
+                return statusCell
+            case 2:
+                let keyFactsCell = detailCollectionView.dequeueReusableCell(withReuseIdentifier: "KeyFactsCollectionViewCell", for: indexPath) as! KeyFactsCollectionViewCell
+                let keyFacts: [(field: String, fact: String)] = [
+                            ("First Flight", "June 4, 2010"),
+                            ("Height", "70 m"),
+                            ("Diameter", "3.7 m"),
+                            ("Mass", "549,054 kg"),
+                            ("Stages", "2"),
+                            ("Payload to LEO", "22,800 kg")
+                        ]
+                
+                keyFactsCell.configure(with: keyFacts)
+                
+                return keyFactsCell
+            case 3:
+                let mapCell = detailCollectionView.dequeueReusableCell(withReuseIdentifier: "MapCollectionViewCell", for: indexPath) as! MapCollectionViewCell
+                return mapCell
+            default:
+                let fallbackCell = UICollectionViewCell()
+                return fallbackCell
+            }
             
         default:
-            //this will be replaced anyway
-            let carouselCell = detailCollectionView.dequeueReusableCell(withReuseIdentifier: "FeaturedCarouselCollectionViewCell", for: indexPath) as! FeaturedCarouselCollectionViewCell
-            carouselCell.carouselImage.image = UIImage(named: "Falcon1")
-            carouselCell.title.text = "hello world"
-            carouselCell.subtitle.text = "hello world"
-            return carouselCell
+            let fallbackCell = UICollectionViewCell()
+            return fallbackCell
         }
-        
         
     }
     
     func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
         
         if kind == UICollectionView.elementKindSectionHeader {
-                let headerView = collectionView.dequeueReusableSupplementaryView(
-                    ofKind: kind,
-                    withReuseIdentifier: "HeaderView",
-                    for: indexPath
-                )
-                
-                if indexPath.section == 4 { // Pictures section
-                    let label = UILabel(frame: CGRect(x: 0, y: 0, width: collectionView.frame.width - 32, height: 50))
-                    label.text = "Pictures"
-                    label.font = UIFont.systemFont(ofSize: 17, weight: .bold)
-                    label.textColor = .white
-                    
-                    headerView.subviews.forEach { $0.removeFromSuperview() } // Clear previous labels
-                    headerView.addSubview(label)
-                }
-                
-                return headerView
+            let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "HeaderView", for: indexPath)
+            
+            let label = UILabel(frame: CGRect(x: 0, y: 0, width: collectionView.frame.width - 32, height: 50))
+            label.font = UIFont.systemFont(ofSize: 17, weight: .bold)
+            label.textColor = .white
+            headerView.subviews.forEach { $0.removeFromSuperview() } // Clear previous labels
+            headerView.addSubview(label)
+            
+            if indexPath.section == 4 { // Pictures section
+                label.text = "Pictures"
             }
             
-            return UICollectionReusableView()
+            if indexPath.section == 3 { // Map section
+                label.text = "Location"
+            }
+                
+            return headerView
+        }
+            
+        return UICollectionReusableView()
     }
 }
